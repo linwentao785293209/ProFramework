2de4accc16d3a5212935c6c80150c53307a925d9
linwentao785293209 <785293209@qq.com>
2024-10-28 15:24:52 +0800

    数据管理模块：数据管理器接口和抽象类

---
+++ Assets/ProFramework/Scripts/Data/IProDataManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProFramework
+{
+    /// <summary>
+    /// 数据管理器接口
+    /// </summary>
+    public interface IProDataManager
+    {
+        void Save<TData>(string key, TData value);
+
+        void Save(string key, object value);
+
+        TData Load<TData>(string key) where TData : class, new();
+
+        object Load(string key, Type type);
+    }
+}
---
+++ Assets/ProFramework/Scripts/Data/ProAbstractDataManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProFramework
+{
+    /// <summary>
+    /// 数据管理器基类
+    /// </summary>
+    /// <typeparam name="T">数据管理器类型</typeparam>
+    public abstract class ProAbstractDataManager<T> : ProCSharpSingleton<T>, IProDataManager where T : ProCSharpSingleton<T>
+    {
+        public abstract void Save<TData>(string key, TData value);
+
+        public abstract void Save(string key, object value);
+
+        public abstract TData Load<TData>(string key) where TData : class, new();
+
+        public abstract object Load(string key, Type type);
+    }
+}
