33fdc2fdc27322892c247863635c59df6f20ab3d
linwentao785293209 <785293209@qq.com>
2024-10-29 14:47:43 +0800

    数据管理模块：PlayerPrefs数据管理器 添加todo 高精度待支持

--- Assets/ProFrameworkTest/Data/PlayerPrefs/PlayerPrefsTest.cs
+++ Assets/ProFrameworkTest/Data/PlayerPrefs/PlayerPrefsTest.cs
@@ -9,6 +9,7 @@ namespace ProFrameworkTest
     {
         private void Update()
         {
+            //todo 高精度不完全支持
             if (Input.GetKeyDown(KeyCode.Q))
             {
                 PlayerPrefsTestClass playerPrefsTestClass1 = new PlayerPrefsTestClass();
