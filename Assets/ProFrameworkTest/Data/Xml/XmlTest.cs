60e508dc701c4bf0691a7ba1e9b45ad4fa08d193
linwentao785293209 <785293209@qq.com>
2024-10-30 22:30:57 +0800

    数据管理模块：Xml管理器测试

---
+++ Assets/ProFrameworkTest/Data/Xml/XmlTest.cs
@@ -0,0 +1,93 @@
+﻿using ProFramework;
+using UnityEngine;
+
+
+namespace ProFrameworkTest
+{
+    public class XmlTest : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                XmlTestClass xmlTestClass1 = new XmlTestClass();
+                xmlTestClass1.intValue = 250;
+                ProXmlDataManager.Instance.Save<XmlTestClass>("myXmlTestClass",
+                    xmlTestClass1);
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                XmlTestClass xmlTestClass2 =
+                    ProXmlDataManager.Instance.Load<XmlTestClass>("myXmlTestClass");
+
+                if (xmlTestClass2 != null)
+                {
+                    // 打印各个类型的值
+                    ProLog.LogDebug($"sbyteValue: {xmlTestClass2.sbyteValue}");
+                    ProLog.LogDebug($"intValue: {xmlTestClass2.intValue}");
+                    ProLog.LogDebug($"shortValue: {xmlTestClass2.shortValue}");
+                    ProLog.LogDebug($"longValue: {xmlTestClass2.longValue}");
+                    ProLog.LogDebug($"byteValue: {xmlTestClass2.byteValue}");
+                    ProLog.LogDebug($"uintValue: {xmlTestClass2.uintValue}");
+                    ProLog.LogDebug($"ushortValue: {xmlTestClass2.ushortValue}");
+                    ProLog.LogDebug($"ulongValue: {xmlTestClass2.ulongValue}");
+                    ProLog.LogDebug($"floatValue: {xmlTestClass2.floatValue}");
+                    ProLog.LogDebug($"doubleValue: {xmlTestClass2.doubleValue}");
+                    ProLog.LogDebug($"decimalValue: {xmlTestClass2.decimalValue}");
+                    ProLog.LogDebug($"boolValue: {xmlTestClass2.boolValue}");
+                    ProLog.LogDebug($"charValue: {xmlTestClass2.charValue}");
+                    ProLog.LogDebug($"stringValue: {xmlTestClass2.stringValue}");
+                    ProLog.LogDebug($"testItemClass: {xmlTestClass2.testItemClass}");
+
+                    // 打印数组
+                    ProLog.LogDebug("intArray:");
+                    foreach (var item in xmlTestClass2.intArray)
+                    {
+                        ProLog.LogDebug($"  {item}");
+                    }
+
+                    // 打印数组
+                    ProLog.LogDebug("XmlTestItemClassArray:");
+                    foreach (var item in xmlTestClass2.XmlTestItemClassArray)
+                    {
+                        ProLog.LogDebug($"  {item}");
+                    }
+
+                    // 打印列表
+                    ProLog.LogDebug("intList:");
+                    foreach (var item in xmlTestClass2.intList)
+                    {
+                        ProLog.LogDebug($"  {item}");
+                    }
+
+                    // 打印字典
+                    ProLog.LogDebug("intStringDict:");
+                    foreach (var item in xmlTestClass2.intStringDict)
+                    {
+                        ProLog.LogDebug($"  Key: {item.Key}, Value: {item.Value}");
+                    }
+
+                    // 打印列表
+                    ProLog.LogDebug("testItemList:");
+                    foreach (var item in xmlTestClass2.testItemList)
+                    {
+                        ProLog.LogDebug($"  {item}");
+                    }
+
+                    // 打印字典
+                    ProLog.LogDebug("testItemDict:");
+                    foreach (var item in xmlTestClass2.testItemDict)
+                    {
+                        ProLog.LogDebug($"  Key: {item.Key}, Value: {item.Value}");
+                    }
+                }
+                else
+                {
+                    ProLog.LogError("加载 XmlTestClass 失败.");
+                }
+
+                ProLog.LogDebug($"{ProXmlDataManager.Instance.PersistentDataPath}");
+            }
+        }
+    }
+}
---
+++ Assets/ProFrameworkTest/Data/Xml/XmlTestClass.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using ProFramework;
+
+namespace ProFrameworkTest
+{
+    public class XmlTestClass
+    {
+        // 有符号类型
+        public sbyte sbyteValue = -50;
+        public int intValue = -123456789;
+        public short shortValue = -30000;
+        public long longValue = -98765432101234L;
+
+        // 无符号类型
+        public byte byteValue = 100;
+        public uint uintValue = 4294967295;
+        public ushort ushortValue = 60000;
+        public ulong ulongValue = 18446744073709551615;
+
+        // 浮点数
+        public float floatValue = 3.14f;
+        public double doubleValue = 1234567890.123456789;
+        public decimal decimalValue = 1234567890123456789012345678.12345678m;
+
+        // 特殊类型
+        public bool boolValue = true;
+        public char charValue = 'A';
+        public string stringValue = "Hello, Xml!";
+
+        // 自定义数据类型
+        public XmlTestItemClass testItemClass = new XmlTestItemClass(123, 456);
+
+        // 集合类型
+        public int[] intArray = new[] { 1, 2, 3, 4, 5 };
+
+        public XmlTestItemClass[] XmlTestItemClassArray = new[]
+        {
+            new XmlTestItemClass(123, 456),
+            new XmlTestItemClass(789, 101112)
+        };
+
+        public List<int> intList = new List<int>() { 1, 2, 3, 4, 5 };
+
+        public ProXmlSerializeDictionary<int, string> intStringDict = new ProXmlSerializeDictionary<int, string>()
+        {
+            { 1, "One" },
+            { 2, "Two" },
+            { 3, "Three" }
+        };
+
+        public List<XmlTestItemClass> testItemList = new List<XmlTestItemClass>()
+        {
+            new XmlTestItemClass(111, 222),
+            new XmlTestItemClass(333, 444)
+        };
+
+        public ProXmlSerializeDictionary<int, XmlTestItemClass> testItemDict = new ProXmlSerializeDictionary<int, XmlTestItemClass>()
+        {
+            { 1001, new XmlTestItemClass(999, 888) },
+            { 1002, new XmlTestItemClass(777, 666) }
+        };
+    
+    }
+}
